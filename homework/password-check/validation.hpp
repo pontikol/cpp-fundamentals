4d55f0e18f7d9e948af66ecd118c4e55f6dbef9f
Pontikol <63785602+pontikol@users.noreply.github.com>
2025-03-23 10:28:37 +0100

    Update validation.hpp

--- homework/password-check/validation.hpp
+++ homework/password-check/validation.hpp
@@ -1,22 +1,19 @@
-// TODO: I'm empty :) Put enum and function headers here.
-// Don't forget the header guard - #pragma once
-#pragma once
-#include <iostream>
+#ifndef VALIDATION_HPP
+#define VALIDATION_HPP
+#include <string>
 
-enum class ErrorCode
-{
-		Ok,
-			PasswordNeedsAtLeastNineCharacters,
-				PasswordNeedsAtLeastOneNumber,
-					PasswordNeedsAtLeastOneSpecialCharacter,
-						PasswordNeedsAtLeastOneUppercaseLetter,
-							PasswordsDoNotMatch
+enum class ErrorCode {
+    Ok = 2,
+    PasswordNeedsAtLeastNineCharacters,
+    PasswordNeedsAtLeastOneNumber,
+    PasswordNeedsAtLeastOneSpecialCharacter,
+    PasswordNeedsAtLeastOneUppercaseLetter,
+    PasswordsDoNotMatch
 };
 
-	
-std::string getErrorMessage(ErrorCode a);  // converts Enum to string (zeby mozna bylo wyswietlic przez std::cout)
-bool doPasswordsMatch(std::string a,std::string b); // sprawdza czy string a jest taki sam jak string b
-ErrorCode checkPasswordRules(std::string a); // jak dasz stringa to zwraca losowa wartosc z enuma wyzej zeby wyswietlic uzyj pierwszej funkcji
-ErrorCode checkPassword(std::string a, std::string b); // sam juz nie pamietam zajzyj do .cpp
+std::string getErrorMessage(ErrorCode test);
+bool doPasswordsMatch(std::string a, std::string b);
+ErrorCode checkPasswordRules(std::string a);
+ErrorCode checkPassword(std::string a, std::string b);
 
-
+#endif
