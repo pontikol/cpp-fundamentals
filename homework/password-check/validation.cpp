d69a2c075ca78d0d713ce757ff01990d3b971087
Pontikol <63785602+pontikol@users.noreply.github.com>
2025-03-23 10:29:11 +0100

    Update validation.cpp

--- homework/password-check/validation.cpp
+++ homework/password-check/validation.cpp
@@ -1,62 +1,56 @@
-#include "validation.hpp"
-// TODO: Put implementations here
-#include <cstdlib>
-#include <ctime>
+	#include "validation.hpp"
+	#include <cstdlib>
+	#include <algorithm>
+	#include  <cctype>
 
-std::string getErrorMessage(ErrorCode a)
-{
-		switch (a)
-				{
-						case ErrorCode::Ok: return "Ok";
-								    	case ErrorCode::PasswordNeedsAtLeastNineCharacters: return "Password needs to have at least nine characters";
-															    	case ErrorCode::PasswordNeedsAtLeastOneNumber: return "Password needs to have at least one number";
-																					       	case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter: return "Password needs to have at least one special character";
-																													 	case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter: return "Password needs to have at least one uppercase letter";
-																																						case ErrorCode::PasswordsDoNotMatch: return "Passwords do not match";
+	std::string getErrorMessage(ErrorCode test)
+	{
+		switch (test) {
+		case ErrorCode::Ok:
+			return "Haslo jest poprawne";
+		case ErrorCode::PasswordNeedsAtLeastNineCharacters:
+			return "Haslo powinno zawierac conajmniej dziewiec znakow";
+		case ErrorCode::PasswordNeedsAtLeastOneNumber:
+			return "Haslo powinno zawierac conajmniej jedna cyfre";
+		case ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter:
+			return "Haslo powinno zawierac conajmniej jeden znak specjalny";
+		case ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter:
+			return "Haslo powinno zwierac conajmniej jedna duza litere";
+		case ErrorCode::PasswordsDoNotMatch:
+			return "Hasla nie sa takie same";
+		default:
+			return "Nieznany blad";
+		}
+	};
 
-																																										     	}
-}
+	bool doPasswordsMatch(std::string a, std::string b) {
 
-bool doPasswordsMatch(std::string a, std::string b)
-{
-		if (a == b)
-				{
-							return true;
-								}
-			else
-					{
-								return false;
-									}
+		return a == b;
+	}
 
-}
+	ErrorCode checkPasswordRules(std::string a) {
+		if (a.size() >= 9) {
+			if(std::any_of(a.begin(), a.end(), [](char x) { return isdigit(x); } )){
+				if (std::any_of(a.begin(), a.end(), [](char x) { return ispunct(x); })) {
+					if (std::any_of(a.begin(), a.end(), [](char x) { return isupper(x); })) {
+						return ErrorCode::Ok;
+					}
+					else { return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter; }
+				}
+				else {
+					return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
+				}
+			}
+			else { return ErrorCode::PasswordNeedsAtLeastOneNumber;}
+		}
+		else {
+			return ErrorCode::PasswordNeedsAtLeastNineCharacters;}
+	}
 
-
-ErrorCode checkPasswordRules(std::string a)
-{
-		int x;
-			srand(time(NULL));
-				x = (rand() % 6) + 1;
-
-					switch (x)
-							{
-									case 1: return ErrorCode::Ok;
-											case 2: return ErrorCode::PasswordNeedsAtLeastNineCharacters;
-													case 3: return ErrorCode::PasswordNeedsAtLeastOneNumber;
-															case 4: return ErrorCode::PasswordNeedsAtLeastOneSpecialCharacter;
-																	case 5: return ErrorCode::PasswordNeedsAtLeastOneUppercaseLetter;
-																			case 6: return ErrorCode::PasswordsDoNotMatch;
-																					}
-
-
-}
-
-ErrorCode checkPassword(std::string a, std::string b)
-{
-
-		if (doPasswordsMatch(a, b))
-					return checkPasswordRules(a);
-			else
-						return ErrorCode::PasswordsDoNotMatch;
-
-}
+	ErrorCode checkPassword(std::string a, std::string b) {
+		if (doPasswordsMatch(a, b)) {
+			return checkPasswordRules(a);
+		}
+		else return ErrorCode::PasswordsDoNotMatch;
+	}
 
