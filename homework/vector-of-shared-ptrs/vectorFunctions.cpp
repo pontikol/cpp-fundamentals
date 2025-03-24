0241f44356c1fac9892ab66d6f8903ea87f32a2e
Pontikol <63785602+pontikol@users.noreply.github.com>
2025-03-24 11:22:17 +0100

    Create vectorFunctions.cpp

---
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -0,0 +1,32 @@
+#include "vectorFunctions.hpp"
+#include <iostream>
+
+std::vector<std::shared_ptr<int>> generate(int count) {
+	std::vector<std::shared_ptr<int>> vec;
+	for (int i = 0; i <= count - 1; i++) {
+		vec.push_back(std::make_shared<int>(i));
+	}
+	return vec;
+}
+
+void print(std::vector<std::shared_ptr<int>> zab) {
+	for (std::shared_ptr<int> n : zab) {
+		std::cout << *n<<std::endl;
+	}
+}
+
+void add10(std::vector<std::shared_ptr<int>>& vec) {
+	for (std::shared_ptr<int> n : vec) {
+		*n += 10;
+	}
+}
+
+void sub10(int * const n) {
+	*n -= 10;
+}
+void sub10(std::vector<std::shared_ptr<int>> vec) {
+	for (std::shared_ptr<int> n : vec) {
+		sub10(n.get());
+	}
+
+}
